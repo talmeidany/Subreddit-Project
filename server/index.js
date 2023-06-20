481da1241e5bcb89d2d76e14e3dfe46fe8aa9a5c
talmeidany <101655450+talmeidany@users.noreply.github.com>
2023-06-20 15:42:01 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -41,11 +41,13 @@ app.get("/", (req, res) => {
    
     .catch((error) => {
   // Handle different error cases and send appropriate error responses
-   if (error.response && error.response.status === 404) {
-    return res.status(404).json({ error: "Subreddit not found." }).end();
-  } else  {
-    return res.status(403).json({ error: "Forbidden Subreddit." }).end();
-  }
+  if (error.posts.status === 404) {
+        return res.status(404).json({ error: "Subreddit not found." }).end;
+      } else if (error.posts.status === 403) {
+        return res.status(403).json({ error: "Forbidden Subreddit." }).end;
+      } else {
+        return res.status(500).json({ error: "Internal server error." }).end;
+      }
 });
 });
 
