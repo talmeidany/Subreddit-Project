2f752101aba7e596e7cbda713a078dfd981e5e4c
talmeidany <101655450+talmeidany@users.noreply.github.com>
2023-06-22 10:15:44 +0300

    Update Search.js

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -3,9 +3,10 @@ import "./Search.css"
 
 function Search(props) {
   const [subreddit, setSubreddit] = useState('');
+  const [filterWord, setFilterWord] = useState('');
 
   const handleSearch = () => {
-    props.findSubreddit(subreddit);
+    props.findSubreddit(subreddit, filterWord);
     setSubreddit('');
   };
 
@@ -17,10 +18,15 @@ function Search(props) {
         readOnly
       />
       <div className="search-container">
-        <input className="userText"
+        <input className="text"
           placeholder="Enter Subreddit..."
           value={subreddit}
           onChange={e => setSubreddit(e.target.value)}
+        />
+         <input className="text"
+          placeholder="Enter a filter word..."
+          value={filterWord}
+          onChange={e => setFilterWord(e.target.value)}
         />
         <button className="button"
           onClick={handleSearch}
