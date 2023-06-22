01037310fa1c2f7e29bbffda733caa9fb051dbe5
talmeidany <101655450+talmeidany@users.noreply.github.com>
2023-06-22 10:14:07 +0300

    Update Post.js

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -13,19 +13,25 @@ function Post({ post }) {
       clicked: bol
     }));
   }
+   // Function to handle the click event and update the 'like' value
+ function handleLike() {
+  setPost(prevPost => ({
+    ...prevPost,
+    like: !prevPost.like
+  }));
+}
+ 
   return (
     <div className={updatePost.clicked ? 'card_clicked' : 'card'}>
+      <button className={updatePost.like ? 'like_btn_clicked' : 'like_btn'} onClick={(e) => handleLike()} >Like</button>    
       <h2 className='card__title'>{updatePost.title}</h2>
       <p className='card__score' >score: {updatePost.score}</p>
       <div className='buttons_container'>
-        <a className="card__btn" href={baseURL + updatePost.url}
+          <a className="card__btn" href={baseURL + updatePost.url}
           target='_blank' onClick={(e) => handleClick(true)} >Read</a>
-        <button className="btn2" onClick={(e) => handleClick(false)} >UnRead</button>
+          <button className="btn2" onClick={(e) => handleClick(false)} >UnRead</button>
       </div>
     </div>
-
   )
 }
-
-
 export default Post;
